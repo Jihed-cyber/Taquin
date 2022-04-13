22fb6105fc7c8e133e60651bf41cfde198c4c9f2
Jean LE MIRE <prenom.nom@ens.uvsq.fr>
2022-04-13 16:01:46 +0200

    mon programme

--- Programme.py
+++ Programme.py
@@ -1,4 +1,20 @@
+#######################
+# Auteur: Jean Le Mire
+
+########################
+# import des librairies
+
+import tkinter as tk
+import random as rd
 
 
+########################
+# constantes
 
-L=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+############################
+# variables globales
+
+####################
+# fonctions
+
+def shake
