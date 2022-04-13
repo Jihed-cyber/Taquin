03a21eaea9e25c15471d00a78c9f4a9a56cc4eed
Jean LE MIRE <prenom.nom@ens.uvsq.fr>
2022-04-13 17:06:20 +0200

    Première fonction

--- Programme.py
+++ Programme.py
@@ -10,11 +10,40 @@ import random as rd
 
 ########################
 # constantes
+HEIGHT = 400
+WIDTH = 400
 
 ############################
 # variables globales
+taquin = []
+grille= []
 
 ####################
 # fonctions
 
-def shake
+def create_taquin():
+    """Remplit la liste taquin à deux dimension des entiers de 0 à 15 mélangés"""
+    global taquin
+    list_entier = list(range(16))
+    rd.shuffle(list_entier)
+    for i in range(4):
+        taquin.append(list_entier[4*i:4*(i+1)])
+
+
+def display_taquin():
+    """Affiche la liste du taquin dans le canevas sous forme de tableau"""
+
+
+#########################
+# partie principale
+
+# création des widgets
+root = tk.Tk()
+root.title("Taquin")
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+
+# placement des widgets
+canvas.grid(column=1, row=0, rowspan=10)
+
+# boucle principale
+root.mainloop()
